94cc23940debc3a64f08148ce4d531cff73d1716
udhayanandhini529 <udhayanandhini@gmail.com>
2026-02-12 16:22:50 +0530

    success

---
+++ server.py
@@ -0,0 +1,21 @@
+import socket
+port = 60000
+s = socket.socket()
+host = socket.gethostname()
+s.bind((host, port))
+s.listen(5)
+while True:
+    conn, addr = s.accept()
+    data = conn.recv(1024)
+    print('Server received', repr(data))
+    filename='mytext.txt'
+    f = open(filename,'rb')
+    l = f.read(1024)
+    while (l):
+        conn.send(l)
+        print('Sent ',repr(l))
+        l = f.read(1024)
+    f.close()
+    print('Done sending')
+    conn.send('Thank you for connecting'.encode())
+    conn.close()
